ce64d12cc56898e520839e2fa0d6dca7df4a8ae8
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2025-04-10 22:49:06 +0200

    Downsample the train ds for training purposes

--- ex4/data.py
+++ ex4/data.py
@@ -1,7 +1,8 @@
 import os
 import shutil
+import torch
 from torchvision.datasets import ImageFolder
-from torch.utils.data import random_split
+from torch.utils.data import random_split, Subset
 from torch import Generator
 
 
@@ -26,3 +27,10 @@ def save_subset(subset, original_dataset, output_dir):
         dest_dir = os.path.join(output_dir, class_name)
         os.makedirs(dest_dir, exist_ok=True)
         shutil.copy(filepath, dest_dir)
+
+def sample_dataset(dataset, sample_fraction=0.5, seed=42):
+    total_size = len(dataset)
+    sample_size = int(total_size * sample_fraction)
+    generator = Generator().manual_seed(seed)
+    indices = torch.randperm(total_size, generator=generator)[:sample_size].tolist()
+    return Subset(dataset, indices)
